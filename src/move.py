55dc281fcebe6e00501fa4e6657163c988c9e7d3
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-10-30 14:38:20 +0600

    update servo config

--- src/move.py
+++ src/move.py
@@ -4,72 +4,71 @@ import os
 import os.path
 import yaml
 import time
-
-import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-from adafruit_servokit import ServoKit
-
-Motor1 = {'EN': 27, 'input1': 19, 'input2': 16}
-Motor2 = {'EN': 22, 'input1': 26, 'input2': 20}
-
-for x in Motor1:
-    GPIO.setup(Motor1[x], GPIO.OUT)
-    GPIO.setup(Motor2[x], GPIO.OUT)
-
-EN1 = GPIO.PWM(Motor1['EN'], 100)    
-EN2 = GPIO.PWM(Motor2['EN'], 100)    
-
-EN1.start(0)                    
-EN2.start(0)  
+from __future__ import division
 
 
-hand = ServoKit(channels=16)
+import Adafruit_PCA9685
+
+pwm1 = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)
+pwm2 = Adafruit_PCA9685.PCA9685(address=0x41, busnum=1)
+
+servo_min = 100 
+servo_max = 600 
 
 ROOT_PATH = os.path.realpath(os.path.join(__file__, '..', '..'))
 
 def readYaml():
-    with open('{}/src/configuration.yaml'.format(ROOT_PATH),'r+', encoding='utf8') as conf:
-        servo = yaml.load(conf, Loader=yaml.FullLoader)
-    return servo
+    with open('{}/src/servoConfiguration.yaml'.format(ROOT_PATH),'r+', encoding='utf8') as conf:
+        servoConfig = yaml.load(conf, Loader=yaml.FullLoader)
+    return servoConfig
 
 
 def writeYaml(s=None):
-    with open('{}/src/configuration.yaml'.format(ROOT_PATH),'w', encoding='utf8') as conf:
-        if s==None:
-            yaml.dump(servo,conf)
-        else:
+    with open('{}/src/servoConfiguration.yaml'.format(ROOT_PATH),'w', encoding='utf8') as conf:
             yaml.dump(s,conf)
 
 
-servo = readYaml()
+servoConfig = readYaml()
 
-if servo == None:
-    with open('{}/src/configurationBackUp.yaml'.format(ROOT_PATH),'r+', encoding='utf8') as conf:
+if servoConfig == None:
+    with open('{}/src/servoConfigurationBackUp.yaml'.format(ROOT_PATH),'r+', encoding='utf8') as conf:
         servoBackUp = yaml.load(conf, Loader=yaml.FullLoader)
     writeYaml(servoBackUp)
-    servo = readYaml()
-    if servo == None:
+    servoConfig = readYaml()
+    if servoConfig == None:
         print('close')
         exit()
 
 
-Initial = servo['Initial_Position']['I2C']
-Current = servo['Current_Position']['I2C']
-
-InitialGpio = servo['Initial_Position']['Gpio']
-CurrentGpio = servo['Current_Position']['Gpio']
-GpioPin     = servo['Pin']['Gpio']
-I2CPin      = servo['Pin']['I2C']
+Initial = servoConfig['initialPosition']
+Current = servoConfig['currentPosition']
 
 
-for i in range(0,6):
-    GPIO.setup(GpioPin[i], GPIO.OUT)
-Servo = []
-for i in range(0,6):
-    Servo.append(GPIO.PWM(GpioPin[i],50))
-    Servo[i].start(0)
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000    
+    pulse_length //= 60       
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096      
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm1.set_pwm(channel, 0, pulse)
+    pwm2.set_pwm(channel, 0, pulse)
 
+pwm1.set_pwm_freq(60)
+pwm2.set_pwm_freq(60)
+
+def mapRange(value, inMin, inMax, outMin, outMax):
+    return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
+
+def servoMove(n, v):
+    n -= 1
+    v = int(mapRange(v, 0, 180, 100, 600))
+    if(n<16):
+        pwm1.set_pwm(n, 0, v)
+    else:
+        n -= 16
+        pwm2.set_pwm(n, 0, v)
 
 def changeDegree(pin,newDegree,time1=0.05,time2=0):
     maxChange = 0
@@ -84,87 +83,31 @@ def changeDegree(pin,newDegree,time1=0.05,time2=0):
                 Current[pin[i]] -= 5
 
         for i in range(0,pinSize):
-            hand.servo[pin[i]].angle = Current[pin[i]]
-            servo['Current_Position']['I2C'][pin[i]] = Current[pin[i]]
+            servoMove(pin[i], Current[pin[i]])
+            servoConfig['currentPosition'][pin[i]] = Current[pin[i]]
         writeYaml()
         time.sleep(time1)
 
-        
-def takePosition():
-    changeDegree([7,8],[180,0])
-    changeDegree(I2CPin,Initial)
 
 
-def changeDegreeGpio(pin,degree,update,duration):
-    pinSize = len(pin)
-    for i in range(0,pinSize):
-        p = pin[i]
-        if CurrentGpio[p]>degree[i]:
-            update = -update
-
-        for deg in range(CurrentGpio[p],degree[i],update):
-            duty = deg/18
-            duty+=2
-            Servo[p].ChangeDutyCycle(duty)
-            time.sleep(duration)
-        CurrentGpio[p]=degree[i]
-        writeYaml()
 
 
-def Run(a, b, c, d, x):
-    GPIO.output(Motor1['input1'], GPIO.LOW)
-    GPIO.output(Motor1['input2'], GPIO.LOW)
-    GPIO.output(Motor2['input1'], GPIO.LOW)
-    GPIO.output(Motor2['input2'], GPIO.LOW)
-
-    if a==1:
-        GPIO.output(Motor1['input1'], GPIO.HIGH)
-    if b==1:
-        GPIO.output(Motor1['input2'], GPIO.HIGH)
-    if c==1:
-        GPIO.output(Motor2['input1'], GPIO.HIGH)
-    if d==1:
-        GPIO.output(Motor2['input2'], GPIO.HIGH)
-
-    EN2.ChangeDutyCycle(x)
-    EN1.ChangeDutyCycle(x)
 
 
-def Stop():
-    Run(0,0,0,0,0)
 
 
-def Start_Slow(a, b, c, d):
-    for i in range(0,100,20):
-        Run(a,b,c,d,i)
-        time.sleep(0.5)
+
+
+
+
 
     
-def Stop_Slow(a,b,c,d):
-    for i in range(100,0,-20):
-        Run(a,b,c,d,i)
-        time.sleep(0.5)
 
 
-def yes(times=3):
-    for i in range(0,times):
-        changeDegree([0],[30])
-        time.sleep(0.08)
-        changeDegree([0],[0])
-        time.sleep(0.08)
 
-def no(times=3):
-    for i in range(0,times):
-        changeDegreeGpio([0],[70],5,0.05)
-        time.sleep(0.2)
-        changeDegreeGpio([0],[110],5,0.05)
-        time.sleep(0.2)
-    changeDegreeGpio([0],[90],5,0.05)
 
-def move_head(times=3):
-    for i in range(0,times):
-        changeDegree([0],[20])
-        changeDegreeGpio([0],[80],5,0.05)
-        changeDegree([0],[0])
-        changeDegreeGpio([0],[100],5,0.05)
+
+
+
+
 
