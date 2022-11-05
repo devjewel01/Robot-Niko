fbb79aca9f3f2bc01f62b281a6911c55bc7af55d
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-11-05 19:25:07 +0600

    update

--- code/Speak/Speak2.py
+++ code/Speak/Speak2.py
@@ -2,23 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from time import sleep
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 chrome_options = Options()
 chrome_options.add_argument('--log-level=3')
 # chrome_options.headless = True
-PathofDriver = "Driver/chromedriver"
-driver = webdriver.Chrome(PathofDriver,options=chrome_options)
+PathofDriver = Service("Driver/chromedriver")
+driver = webdriver.Chrome(service=PathofDriver,options=chrome_options)
 driver.maximize_window()
 
 Website = 'https://www.naturalreaders.com/online/'
-
 driver.get(Website)
+
 sleep(2)
 driver.find_element(by=By.XPATH, value='//*[@id="pw-reading-scroll"]/div[1]/button').click()
 driver.find_element(by=By.XPATH, value='//*[@id="pw-reading-page"]/div[1]/div/div[2]/app-pw-reading-bar/div/div/button[1]').click()
 sleep(2)
-driver.find_element(by=By.XPATH, value='//*[@id="mat-dialog-0"]/app-pw-voices/mat-dialog-content/div/mat-selection-list/mat-list-option[4]/div/div[2]/div/div[1]').click()
+driver.find_element(by=By.XPATH, value='//*[@id="mat-dialog-0"]/app-pw-voices/mat-dialog-content/div/mat-selection-list/mat-list-option[8]/div/div[2]/div/div[1]').click()
 driver.find_element(by=By.XPATH, value='//*[@id="mat-dialog-0"]/app-pw-voices/div/div/button').click()
 
 def Speak(Text):
--- src/talk.py
+++ src/talk.py
@@ -1,65 +1,50 @@
-#!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-from googletrans import Translator
-from gtts import gTTS
-import os
-import os.path
-import yaml
+chrome_options = Options()
+chrome_options.add_argument('--log-level=3')
+# chrome_options.headless = True
+PathofDriver = Service("Driver/chromedriver")
+driver = webdriver.Chrome(service=PathofDriver,options=chrome_options)
+driver.maximize_window()
 
-ROOT_PATH = os.path.realpath(os.path.join(__file__, '..', '..'))
-USER_PATH = os.path.realpath(os.path.join(__file__, '..', '..','..'))
+Website = 'https://www.naturalreaders.com/online/'
+driver.get(Website)
 
-with open('{}/src/conversation.yaml'.format(ROOT_PATH),'r', encoding='utf8') as conf:
-    custom_conversation = yaml.load(conf, Loader=yaml.FullLoader)
+sleep(2)
+driver.find_element(by=By.XPATH, value='//*[@id="pw-reading-scroll"]/div[1]/button').click()
+driver.find_element(by=By.XPATH, value='//*[@id="pw-reading-page"]/div[1]/div/div[2]/app-pw-reading-bar/div/div/button[1]').click()
+sleep(2)
+driver.find_element(by=By.XPATH, value='//*[@id="mat-dialog-0"]/app-pw-voices/mat-dialog-content/div/mat-selection-list/mat-list-option[8]/div/div[2]/div/div[1]').click()
+driver.find_element(by=By.XPATH, value='//*[@id="mat-dialog-0"]/app-pw-voices/div/div/button').click()
 
-TTSChoice='GTTS'
+def say(Text):
 
-translator = Translator()
-femalettsfilename="/tmp/female-say.mp3"
-malettsfilename="/tmp/male-say.wav"
-ttsfilename="/tmp/gcloud.mp3"
-language='en-US'
-translanguage=language.split('-')[0]
-gender='Female'
+    try:
+        driver.find_element(by=By.XPATH, value='//*[@id="pw-reading-scroll"]/div[1]/button').click()
+        sleep(1)
 
+    except:
+        pass
 
+    Data = str(Text)
+    xpathtec = '//*[@id="inputDiv"]'
+    driver.find_element(by=By.XPATH, value=xpathtec).click()
+    driver.find_element(by=By.XPATH, value=xpathtec).send_keys(Data)
+    driver.find_element(by=By.XPATH, value='//*[@id="pw-reading-page"]/div[1]/div/div[2]/app-pw-reading-bar/div/div/button[3]').click()
+    sleep(2)
 
-#gTTS
-def gttssay(phrase,saylang,specgender):
-    tts = gTTS(text=phrase, lang=saylang)
-    tts.save(femalettsfilename)
-    
-    os.system('sox ' + femalettsfilename + ' ' + malettsfilename + ' pitch -450')
-    os.remove(femalettsfilename)
-    os.system('aplay ' + malettsfilename)
-    os.remove(malettsfilename)
-    
+    try:
+        driver.find_element(by=By.XPATH, value='//*[@id="pw-reading-scroll"]/div[1]/button').click()
+        sleep(1)
 
+    except:
+        pass
 
-def trans(words,destlang,srclang):
-    transword= translator.translate(words, dest=destlang, src=srclang)
-    transword=transword.text
-    transword=transword.replace("Text, ",'',1)
-    transword=transword.strip()
-    print(transword)
-    return transword
-
-
-
-#Text to speech converter with translation
-def say(words,sourcelang=None,destinationlang=None):
-    if sourcelang!=None and destinationlang!=None:
-        
-        sayword=trans(words,destinationlang,sourcelang)
-        gttssay(sayword,translanguage,gender)
-    else:
-        if sourcelang==None:
-            sourcelanguage='en'
-        else:
-            sourcelanguage=sourcelang
-        if sourcelanguage!=translanguage:
-            sayword=trans(words,translanguage,sourcelanguage)
-        else:
-            sayword=words
-        
-        gttssay(sayword,translanguage,gender)
+    print("")
+    print(f" Robot-Niko : {Text}.")
+    print("")
