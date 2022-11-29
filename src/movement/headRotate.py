1f9cb1f00126dc7f7b7845cde761d216b6b6fbce
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-11-29 14:59:28 +0600

    first step for movement

---
+++ src/movement/headRotate.py
@@ -0,0 +1,10 @@
+from move import servoMove
+from time import sleep
+
+servoMove(11, 90)
+sleep(3)
+servoMove(11, 120)
+sleep(3)
+servoMove(11, 50)
+sleep(3)
+servoMove(11, 50)
---
+++ src/movement/move.py
@@ -0,0 +1,6 @@
+
+
+import sys
+sys.path.append('/home/pi/Robot-Niko/src')
+
+from move import *
