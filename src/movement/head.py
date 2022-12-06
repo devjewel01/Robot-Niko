8f50b253966fdb5f99c1bff76edfb220f5a57126
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-12-06 07:49:59 +0600

    head rotation fix

--- src/movement/head.py
+++ src/movement/head.py
@@ -1,19 +1,19 @@
 from move import servoMove
 from time import sleep
-t = 0.005
+t = 0.1
 
 def rotate():
     # servoMove(11, 0)
-    for d in range(90, 0, -1):
+    for d in range(90, 30, -1):
         servoMove(11, d)
         sleep(t)
     sleep(1)
     # servoMove(11, 180)
-    for d in range(0, 180):
+    for d in range(0, 150):
         servoMove(11, d)
         sleep(t)
     sleep(1)
-    for d in range(180, 90, -1):
+    for d in range(150, 90, -1):
         servoMove(11, d)
         sleep(t)
     #servoMove(11, 90)
