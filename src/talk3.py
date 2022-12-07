c022c8cce1734fe019b803e9efc4a9b0beb96ab0
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-12-08 01:18:10 +0600

    update

---
+++ src/talk3.py
@@ -0,0 +1,55 @@
+
+#!/home/pi/robotEnv/bin python 
+
+import os
+import os.path
+import yaml
+
+
+ROOT_PATH = os.path.realpath(os.path.join(__file__, '..', '..'))
+USER_PATH = os.path.realpath(os.path.join(__file__, '..', '..','..'))
+
+with open('{}/src/conversation.yaml'.format(ROOT_PATH),'r', encoding='utf8') as conf:
+    custom_conversation = yaml.load(conf, Loader=yaml.FullLoader)
+
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('window-size=1920x1480')
+
+PathofDriver = Service("/usr/bin/chromedriver")
+driver = webdriver.Chrome(service=PathofDriver,options=chrome_options)
+driver.maximize_window()
+
+
+Website = "https://text-to-speech.org/"
+driver.get(Website)
+
+
+def say(Text):
+    try:
+        driver.find_element(by=By.XPATH, value='//*[@id="text_to_convert"]').clear()
+        sleep(0.5)
+
+    except:
+        pass
+
+    Data = str(Text)
+    xpathtec = '//*[@id="text_to_convert"]'
+    driver.find_element(by=By.XPATH, value=xpathtec).click()
+    driver.find_element(by=By.XPATH, value=xpathtec).send_keys(Data)
+    sleep(0.5)
+    driver.find_element(by=By.XPATH, value='//*[@id="convertbtn"]').click()
+
+    print("")
+    print(f" Robot-Niko Answer : {Text}.")
+    print("")
+
