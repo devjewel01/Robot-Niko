4eaea5dc5c9b2392324799c5ed33eb4ed2745e48
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-12-08 10:55:40 +0600

    test

--- src/faceRecognition.py
+++ src/faceRecognition.py
@@ -11,12 +11,18 @@ def detect(name):
     if appear.count(name)==0:
         print("first time found")
         appear.append(name)
-        if name == "togor hasan" or name=="togor hasan":
+        if name == "togor hasan":
             cmd = "Hello Honorable togor hasan sir, the owener of halima group"
             say(cmd)
             handshake()
             say("it's a honour to have you with us... Have a beautiful day sir")
-            
+        elif name == "DC sir":
+            say("Hello Honoralble Kamrul hasan sir, I recently got the news that you have been promoted as a join secretary at Cabinet Division, congratalations sir, I am very glad to hear this news. Thank you for being with us. you will be miss")
+            handshake()
+        elif name == "shamim alom":
+            cmd = ""
+            say(cmd)
+            handshake()
         else:
             cmd = "Hello " + name + " sir, I am now able to perfectly recognise you...,  how are you sir, I am happy to see you"
             say(cmd)
