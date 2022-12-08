51181a4440484ab60dbb2d845ed7d06a900d2d35
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-12-08 09:43:19 +0600

    update

--- src/speech.py
+++ src/speech.py
@@ -58,42 +58,31 @@ speakingModeOn()
 
 def a1():
     say("Hello World")
-    rotateHead()
-    sleep(5)
 
 def a2():
-    say(" At first, i want to express my warm gratatude to our honourable DC kamrul hasan sir........because of his creative thinking It was possible for me to be here.")
-    omoplateUp()
-    sleep(1)
-    omoplateDown()
+    say("I am robot niko... version 1.0. speed 1.5 Giga Heartz...,  64-bit quad-core ARM  processor..... with 8 Giga Byte RAM and  120 Giga byte  ROM")
 
 def a3():
-    say("I am robot niko... version 1.0. speed 1.5 Giga Heartz...,  64-bit quad-core ARM  processor..... with 8 Giga Byte RAM and  120 Giga byte  ROM")
-    bicepUp()
-    sleep(1)
-    bicepDown()
-
-
+    say(" I have 29 Degree of Freedoms for arm movement..... I Have a 7 inch display for operating me...... I have two high torque motor for my mobility.")
 
 def a4():
-    say(" I have 29 Degree of Freedoms for arm movement..... I Have a 7 inch display for operating me...... I have two high torque motor for my mobility.")
-    
-def a5():
     say("I am created by a group of aspired students from Comilla university  named quanta robotics make it happen.......I want mention thier name. they are shanjit Mondol from physics department, Jewel Nath from ICT department, Anik chakraborty from ICT department, Tawsif bin parves from ICT department, Mahin Khan from ICT department")
 
-def a6():
+def a5():
     say(" Let's talk about my features... I can generate any answer for your random questions...... I can take fruitful dicisions in any situations based on my computitions")
 
-def a7():
+def a6():
     say("I can recognize humans and differentiate individual person..... I can be controlled remotely ..... I can learn through my environment, although i am stilling my learning phase..... I am tring to be a great assistants to the humans")
 
-def a8():
-    say("I am  created with a scientific goal in mind.....  Until now It was challenging to create a body of a robot, since it required complex,expensive manufacturing methods.....")
-def a9():
-    say("In just a few days, it is now possible to experiment with different body shapes thanks to 3D printing, which is used to create my body. .... As the government of bangladesh as emphasising on 4th Industrial revolation... I can said proudly that, I am the live 4th industrial revolution")
+def a7():
+    say("I am  created with a scientific goal in mind.....  Until now It was challenging to create a body of a robot, since it required complex, expensive manufacturing methods..... In just a few days, it is now possible to experiment with different body shapes thanks to 3D printing, which is used to create my body. I am bangladesh first 100% home manufactured 3D printed robot.... As the government of bangladesh as emphasising on 4th Industrial revolation... I can said proudly that, I am the live 4th industrial revolution")
 
-def a10():
-    say("I am bangladesh first 100% home manufactured 3D printed robot. Thank  you  all")
+def fifa():
+    #sleep(3)
+    say("I wish to see argentina as a champion because I am a huge fan of Lionel messi")
+    sleep(3)
+    say("but according to probability there is a high chance of brazil become the champion")
+
 
 
 
